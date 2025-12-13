45c6184a4091eb8cafc700643568d12f0313c6c7
Sofia <itssonyamisyura@gmail.com>
2025-12-13 22:20:01 +0100

    работа со слайдерами на странице

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
 
 // скрипты начинают выполнятся когда готова dom структура(не включается загрузка стилей, изобр..)
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     
     modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
 });
