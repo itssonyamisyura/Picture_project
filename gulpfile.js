9404776846684e32449b56c10535f0e60d7101cf
Sofia <itssonyamisyura@gmail.com>
2026-01-01 19:04:50 +0100

    работа с формами отправки данных

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-const dist = "./dist/";
-
+// const dist = "./dist/";
+const dist = "/Applications/MAMP/htdocs/Picture_project";
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
                 .pipe(gulp.dest(dist))
