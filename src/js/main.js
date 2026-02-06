cca109e13ef6af35e2a779c47769942dfe2f583a
Sofia <itssonyamisyura@gmail.com>
2026-02-06 19:23:39 +0100

    калькулятор расчета общей суммы

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
-import showMoreStyles from "./modules/showMoreStyles";
+import showMoreStyles from "./modules/showMoreStyles"
+import calc from "./modules/calc";
 
 // скрипты начинают выполнятся когда готова dom структура(не включается загрузка стилей, изобр..)
 window.addEventListener('DOMContentLoaded', () => {
@@ -17,4 +18,5 @@ window.addEventListener('DOMContentLoaded', () => {
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
     showMoreStyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
