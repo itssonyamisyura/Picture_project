f09787f8315bebd3c5fda37045e8b76eacb583da
Sofia <itssonyamisyura@gmail.com>
2026-02-08 20:42:32 +0100

    scrolling with *Request Animation Frame*

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -13,64 +13,106 @@ const scrolling = (upSelector) => {
         }
     });
 
-    //плавный скролл
-    const element = document.documentElement,
-          body = document.body;
-        
-    const calcScroll = () => {
-        upElem.addEventListener('click', function(event) {
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
-            
-            if (this.hash !== '') {
-                event.preventDefault();
-                let hashElement = document.querySelector(this.hash),
-                // or document.getElementById(this.hash.substring(1)), //получаем строчку без #
-                    hashElementTop = 0;
+    // Scrolling with Request Animation Frame
 
-                while (hashElement.offsetParent){ 
-                    //offsetParent-элемент, относительно которого будет hashElement
-                    //offsetTop-сколько px до верхней границы родительского элемента
-                    hashElementTop += hashElement.offsetTop;
-                    hashElement = hashElement.offsetParent;
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.1;
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
                 }
 
-                hashElementTop = Math.round(hashElementTop);
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock)); 
+                    // кол-во px, которое нужно пролистать в течение анимации
 
-                smoothScroll(scrollTop, hashElementTop, this.hash);
+                    document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
             }
         });
-    };
+    });
 
-    const smoothScroll = (from, to, hash) => {
-        let timeInterval = 1,
-            prevScrollTop,
-            speed;
+
+
+
+    // Pure JS scrolling
+
+    //плавный скролл - в разных браузерах scroll хранится либо тут, либо тут.
+    // const element = document.documentElement,
+    //       body = document.body;
         
-        if (to > from) {
-            speed = 30; //сверху вниз
-        } else {
-            speed = -30;
-        }
+    // const calcScroll = () => {
+    //     upElem.addEventListener('click', function(event) {
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+            
+    //         if (this.hash !== '') {
+    //             event.preventDefault();
+    //             let hashElement = document.querySelector(this.hash), //получаем элемент, к которому нужно проскроллить
+    //             // or document.getElementById(this.hash.substring(1)), //получаем строчку без #
+    //                 hashElementTop = 0;
 
-        let move = setInterval(function() {
-            let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    //             while (hashElement.offsetParent){ 
+    //                 //Сколько пикселей от верха страницы до элемента
+    //                 //offsetParent-элемент, относительно которого будет hashElement
+    //                 //offsetTop-сколько px до верхней границы родительского элемента
+    //                 hashElementTop += hashElement.offsetTop;
+    //                 hashElement = hashElement.offsetParent;
+    //             }
 
-            if (
-                prevScrollTop === scrollTop || 
-                (to > from && scrollTop >= to) ||
-                (to < from && scrollTop <= to)
-            ) {
-                clearInterval(move);
-                history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash);
-            } else {
-                body.scrollTop += speed;
-                element.scrollTop += speed;
-                prevScrollTop = scrollTop;
-            }
-        }, timeInterval);
-    };
-    
-    calcScroll();
+    //             hashElementTop = Math.round(hashElementTop);
+
+    //             smoothScroll(scrollTop, hashElementTop, this.hash);
+    //         }
+    //     });
+    // };
+
+    // const smoothScroll = (from, to, hash) => {
+    //     let timeInterval = 1, //каждые 1мс двигаем страницу
+    //         prevScrollTop,
+    //         speed;
+        
+    //     if (to > from) {
+    //         speed = 30; //сверху вниз
+    //     } else {
+    //         speed = -30;
+    //     }
+
+    //     let move = setInterval(function() {
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+
+    //         if (
+    //             prevScrollTop === scrollTop || 
+    //             (to > from && scrollTop >= to) ||
+    //             (to < from && scrollTop <= to)
+    //         ) {
+    //             clearInterval(move);
+    //             history.replaceState(history.state, document.title, location.href.replace(/#.*$/g, '') + hash); //Добавляет #top в адресную строку, но без перезагрузки страницы
+    //         } else {
+    //             body.scrollTop += speed;
+    //             element.scrollTop += speed;
+    //             prevScrollTop = scrollTop;
+    //         }
+    //     }, timeInterval);
+    // };
+
+    // calcScroll();
 };
 
 export default scrolling;
